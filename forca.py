ea5310296a248b1d6fe0a36233abebbf44b3d644
Guilherme Lourenço <53027213+egmerson@users.noreply.github.com>
2022-07-14 23:10:30 -0300

    bloco 02 continuação

--- forca.py
+++ forca.py
@@ -1,9 +1,35 @@
-#jogo da forca
+#jogo da forca 1:47:49
 
 def jogar():
     print('='*35)
     print('Bem vindo ao jogo de forca')
     print('='*35)
+
+    palavra_secreta = 'banana'
+    letras_acertadas = ['_','_','_','_','_','_']
+
+    enforcou = False
+    acertou = False
+
+    #print para mostrar o tamanho da palavra
+    print(letras_acertadas)
+
+    #Condição do while não enforcou e não acertou
+    while (not enforcou and not acertou ):
+
+        chute = str(input('Qual letra? '))
+        #O metodo .strip() remove espaços antes e depois
+        chute = chute.strip()
+
+        #Index garda a posição da letra
+        index = 0
+        for letra in palavra_secreta:
+            #O metodo .upper deixa em maiúsculo, assim se o usuário digitar maiúsculo não tem problema.
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index +1
+
+        print(letras_acertadas)
 
     print('Fim de jogo')
 
