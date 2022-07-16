6fdb102cb7cc070b43a25c8f67dd107e29a95bcd
Guilherme Lourenço <53027213+egmerson@users.noreply.github.com>
2022-07-16 20:08:21 -0300

    fim do bloco 02

--- forca.py
+++ forca.py
@@ -1,12 +1,77 @@
 #jogo da forca 1:47:49
 
-def jogar():
+#impotando a biblioteca random
+import random
+
+def imprime_mensagem_de_abertura():
     print('='*35)
     print('Bem vindo ao jogo de forca')
     print('='*35)
 
-    palavra_secreta = 'banana'
-    letras_acertadas = ['_','_','_','_','_','_']
+
+def carrega_palavra_secreta():
+    #Abrindo o arquivo para leitura
+    arquivo = open('palavras.txt','r')
+
+    #criando uma lista para receber as palavras do arquivo
+    palavras = []
+
+    #fazendo um laço para ler cada linha do arquivo
+    for linha in arquivo:
+        #limpando a linha, retirando caracteres que não vamo usar
+        linha = linha.strip()
+        
+        #adicionando ao final da minha lista (append()) cada palavras
+        palavras.append(linha)
+
+    #Fechamento do arquivo
+    arquivo.close()
+
+    #Escolnhendo aleatóriamente as palavras dentro do arquivo
+    numero = random.randrange(0,len(palavras))
+
+
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra_secreta):
+    lista = ['_' for letra in palavra_secreta]
+    return lista
+
+
+def pede_chute():
+    chute = str(input('Qual letra? '))
+    #O metodo .strip() remove espaços antes e depois
+    chute = chute.strip().upper()
+    return chute
+
+
+def marca_chute_correto(chute, palavra_secreta, letras_acertadas):
+    #Index garda a posição da letra
+    index = 0
+    for letra in palavra_secreta:
+        #O metodo .upper deixa em maiúsculo, assim se o usuário digitar maiúsculo não tem problema.
+        if(chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
+
+
+def imprime_mensagem_vencedor():
+    print('Parabéns, você ganhou! ')
+
+
+def imprime_mensagem_perdedor():
+    print('Você perdeu" : ( ')
+
+def jogar():
+
+    imprime_mensagem_de_abertura()
+
+    palavra_secreta = carrega_palavra_secreta()
+    
+    #Prenche a lista letras_acertadas com '_' do tamanho da palavra!
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -18,28 +83,22 @@ def jogar():
     #Condição do while não enforcou e não acertou
     while (not enforcou and not acertou ):
 
-        chute = str(input('Qual letra? '))
-        #O metodo .strip() remove espaços antes e depois
-        chute = chute.strip()
+        chute = pede_chute()
 
         #verificando se a letra está na palavras
-
         if(chute in palavra_secreta):
-            
-            #Index garda a posição da letra
-            index = 0
-            for letra in palavra_secreta:
-                #O metodo .upper deixa em maiúsculo, assim se o usuário digitar maiúsculo não tem problema.
-                if(chute.upper() == letra.upper()):
-                    letras_acertadas[index] = letra
-                index = index +1
+            marca_chute_correto(chute, palavra_secreta, letras_acertadas)
         else:
-            erros = erros + 1
+            erros += 1
         
         enforcou = erros == 6
+        acertou = '_' not in letras_acertadas
         print(letras_acertadas)
 
-    print('Fim de jogo')
+    if(acertou):
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor()
 
 if(__name__ == '__main__'):
     jogar()
