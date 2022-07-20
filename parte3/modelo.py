7fddd279c186f4ee8c62d2056e056b9aa9dcad77
Guilherme Lourenço <53027213+egmerson@users.noreply.github.com>
2022-07-19 23:31:01 -0300

    bloco 04 continuação

--- parte3/modelo.py
+++ parte3/modelo.py
@@ -1,22 +1,59 @@
 #parada bloco4 1:05:10
 
-class Filme:
-    
-    def __init__(self, nome, ano, duracao):
-        self.nome = nome
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
+        self._likes = 0
 
-class Serie:
+    @property
+    def likes(self):
+        return self._likes
+
+    @property
+    def nome(self):
+        return self._nome
+
+    def dar_like(self):
+        self._likes +=1
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def imprime(self):
+        print('{} - {} - {}'.format(self._nome, self.ano, self._likes))
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
     
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} - {self.duracao}'
+
+
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} - {self.temporadas}'
+
+class Playlist:
+
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    def tamanho(self):
+        return len(self.programas)
 
 vingadores = Filme('Vingadores - Guerra infinita', 2018, 160)
-print('Nome: {} - Ano: {} - Duração: {}'.format(vingadores.nome, vingadores.ano, vingadores.duracao))
-
 atlanta = Serie('Atlanta', 2018, 2)
-print('Nome: {} - Ano: {} - Temporadas: {}'.format(atlanta.nome, atlanta.ano, atlanta.temporadas))
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
